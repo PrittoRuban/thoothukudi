f518c90259e51dbbbbd2225e4a210b18e9756a45
kirthiSaiT <kirthisai251@gmail.com>
2025-03-26 13:21:10 +0530

    pritto konjum pani kudu da

--- backend/models/Admin.js
+++
@@ -1,7 +0,0 @@
-const AdminSchema = new mongoose.Schema({
-    phNumber: { type: String, required: true, unique: true },
-    policeIdNumber: { type: String, required: true, unique: true },
-    policeIdImage: { type: String, required: true },
-    password: { type: String, required: true },
-  });
-  module.exports = mongoose.model("Admin", AdminSchema);
